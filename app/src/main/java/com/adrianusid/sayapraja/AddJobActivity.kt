ad7428bdc98eec582c952b14aab169fc5b78717e
Reyhan <reyhan18dm@gmail.com>
2022-11-23 00:36:19 +0700

    Layout detail, add job, register

---
+++ app/src/main/java/com/adrianusid/sayapraja/AddJobActivity.kt
@@ -0,0 +1,11 @@
+package com.adrianusid.sayapraja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddJobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_job)
+    }
+}
